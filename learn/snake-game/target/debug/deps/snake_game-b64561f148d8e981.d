ddcb4973fe7d7bf3eb53b65bf4deb9103f0de26c
harry427903 <03yadavanurag@gmail.com>
2024-01-16 23:32:03 +0530

    modified patterns

---
+++ learn/snake-game/target/debug/deps/snake_game-b64561f148d8e981.d
@@ -0,0 +1,5 @@
+/home/olivia/Documents/coding/rust/learn/snake-game/target/debug/deps/snake_game-b64561f148d8e981: src/main.rs
+
+/home/olivia/Documents/coding/rust/learn/snake-game/target/debug/deps/snake_game-b64561f148d8e981.d: src/main.rs
+
+src/main.rs:
---
+++ learn/snake-game/target/debug/snake-game.d
@@ -0,0 +1,1 @@
+/home/olivia/Documents/coding/rust/learn/snake-game/target/debug/snake-game: /home/olivia/Documents/coding/rust/learn/snake-game/src/main.rs
