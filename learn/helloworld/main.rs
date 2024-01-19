14da11d7ffb40d593146ec7eb6ca26d298af09d3
harry427903 <03yadavanurag@gmail.com>
2024-01-19 23:14:57 +0530

    learning

--- learn/helloworld/main.rs
+++ learn/helloworld/main.rs
@@ -1,4 +1,19 @@
 fn main() {
     println!("Hello From the Rust");
     // This is comment
+
+    this is hello world 
+
+    println("hello world ")
+
+    arr[] = {1,2,3,4,fn main() {
+    println!("Hello From the Rust");
+    // This is comment
+
+    // this is hello world 
+
+    println!("hello world");
+
+    let arr: [i32; 4] = [1, 2, 3, 4];
+}}
 }
