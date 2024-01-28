3c682fc24495d8edbf245ef363344fc0ad9afb8f
harry427903 <03yadavanurag@gmail.com>
2024-01-29 00:57:16 +0530

    missing ones

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,31 +1,10 @@
-enum Status {
-    Rich,
-    Poor,
-}
-
-enum Work {
-    Civilian,
-    Soldier,
-}
-enum User {
-    name: String,
-    age: u8,
-    
-}
 fn main() {
-    use crate::Status::{Poor, Rich};
+    println!(:nv)
+}
 
-    use crate::Work::*;
 
-    let status = Poor;
-    let work = Civilian;
-
-    match status {
-        Rich => println!("The rich have lots of money"),
-        Poor => println!("The Poor have no money"),
-    }
-    match work {
-        Civilian => println!("Civilians work!"),
-        Soldier => println!("Soldiers fight!"),
-    }
+fn (int a, int b) add(a, b) {
+    
+    helloworld = "Hello World"
+    ?kkkk
 }
